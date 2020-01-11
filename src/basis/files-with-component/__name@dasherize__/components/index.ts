5b2b6a85707b8e75c3c13435a934d2d6fb9d3110
전상현 <shock@docs.kr>
2020-01-11 09:11:46 +0900

    패키지 정리

--- src/basis/files-with-component/__name@dasherize__/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './grid.component';
-export * from './grid-control.component';
-export * from './form-popup.component';
-export * from './search-area.component';
--- src/basis/schema.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Schema {
-  name: string;
-  title: string;
-  targetPath: string;
-  withComponent: string;
-  project?: string;
-}
